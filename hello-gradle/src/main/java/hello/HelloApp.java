27b485b7fd88cc78c4c0282ed3e629cec560177a
Nicky Moelholm <moelholm@gmail.com>
2015-11-05 11:03:18 +0100

    tidy up

--- hello-gradle/src/main/java/hello/HelloApp.java
+++ hello-gradle/src/main/java/hello/HelloApp.java
@@ -3,12 +3,7 @@ package hello;
 import org.apache.commons.lang.StringUtils;
 
 public class HelloApp {
-
-
-	public static void main (String ... args) {
-
-		System.out.println( StringUtils.capitalize( "Hello World" ) );
-
+	public static void main(String... args) {
+		System.out.println(StringUtils.capitalize("Hello World"));
 	}
-
 }
--- hello-gradle/src/test/java/hello/HelloTest.java
+++ hello-gradle/src/test/java/hello/HelloTest.java
@@ -5,13 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HelloTest {
-
-
 	@Test
 	public void helloTest() {
-
 		assertEquals(42, 42);
-
 	}
-
 }
