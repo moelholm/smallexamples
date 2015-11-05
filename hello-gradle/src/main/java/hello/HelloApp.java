cf51638c99e92f323cd2bc747d089ad04e7b9e71
Nicky Moelholm <moelholm@gmail.com>
2015-11-05 10:32:22 +0100

    added java source, gitignore and an updated gradle file

---
+++ hello-gradle/src/main/java/hello/HelloApp.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.apache.commons.lang.StringUtils;
+
+public class HelloApp {
+
+
+	public static void main (String ... args) {
+
+		System.out.println( StringUtils.capitalize( "Hello World" ) );
+
+	}
+
+}
