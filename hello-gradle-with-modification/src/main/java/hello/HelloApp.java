9413eb7531b26535249b8cc09d12a8e4d5ff5adc
Nicky Moelholm <moelholm@gmail.com>
2015-11-05 13:47:33 +0100

    initial import

---
+++ hello-gradle-with-modification/src/main/java/hello/HelloApp.java
@@ -0,0 +1,9 @@
+package hello;
+
+import org.apache.commons.lang.StringUtils;
+
+public class HelloApp {
+	public static void main(String... args) {
+		System.out.println(StringUtils.capitalize("hello world"));
+	}
+}
---
+++ hello-gradle-with-modification/src/test/java/hello/HelloTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+	@Test
+	public void helloTest() {
+		assertEquals(42, 42);
+	}
+}
