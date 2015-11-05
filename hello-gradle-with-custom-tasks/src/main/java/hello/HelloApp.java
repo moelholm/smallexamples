a6462c7b7659ebbee78a74c1316b68422008c899
Nicky Moelholm <moelholm@gmail.com>
2015-11-05 13:30:24 +0100

    added 'run' support

--- hello-gradle-with-custom-tasks/src/main/java/hello/HelloApp.java
+++ hello-gradle-with-custom-tasks/src/main/java/hello/HelloApp.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang.StringUtils;
 
 public class HelloApp {
 	public static void main(String... args) {
-		System.out.println(StringUtils.capitalize("Hello World"));
+		System.out.println(StringUtils.capitalize("hello world"));
 	}
 }
