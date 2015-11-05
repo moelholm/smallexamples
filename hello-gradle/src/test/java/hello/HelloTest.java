a024d09444ae51c2e90b2765d55c784826ec3860
Nicky Moelholm <moelholm@gmail.com>
2015-11-05 10:53:38 +0100

    added some junit dependent code

--- hello-gradle/src/test/java/hello/HelloTest.java
+++ hello-gradle/src/test/java/hello/HelloTest.java
@@ -1,9 +1,17 @@
 package hello;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class HelloTest {
 
 
+	@Test
+	public void helloTest() {
 
+		assertEquals(42, 42);
+
+	}
 
 }
