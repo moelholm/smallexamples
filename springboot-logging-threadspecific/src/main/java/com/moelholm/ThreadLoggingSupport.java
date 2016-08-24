a7ec4f2a2704387fed7e1cf2d98201fd08a19bc6
Nicky Mølholm <nicky.moelholm@LEGO.com>
2016-08-24 22:44:04 +0200

    Replaced use of ThreadLocal if normal Map

--- springboot-logging-threadspecific/src/main/java/com/moelholm/ThreadLoggingSupport.java
+++ springboot-logging-threadspecific/src/main/java/com/moelholm/ThreadLoggingSupport.java
@@ -1,23 +1,23 @@
 package com.moelholm;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class ThreadLoggingSupport {
 
-    private static ThreadLocal<Boolean> ENABLED = new ThreadLocal<Boolean>() {
-        @Override
-        protected Boolean initialValue() {
-            return Boolean.FALSE;
-        }
-    };
+    private static final Map<Long, Boolean> THREAD_TO_ENABLED = new HashMap<>();
 
     public static void logEverything(boolean enabled) {
-        ENABLED.set(enabled);
+        THREAD_TO_ENABLED.put(Thread.currentThread().getId(), enabled);
     }
 
     public static boolean shouldLogEverything() {
-        return ENABLED.get();
+        return Optional.ofNullable(THREAD_TO_ENABLED.get(Thread.currentThread().getId()))
+                .orElse(false);
     }
 
     public static void cleanup() {
-        ENABLED.remove();
+        THREAD_TO_ENABLED.remove(Thread.currentThread().getId());
     }
 }
