1e1375037c17e0d1ee5de95fb407d67c5b6d4006
Nicky Mølholm <nicky.moelholm@LEGO.com>
2016-10-09 21:21:12 +0200

    renamed config file

--- hello-spring43-injectionpoint/src/main/java/com/moelholm/MyBeanConfig.java
+++ hello-spring43-injectionpoint/src/main/java/com/moelholm/MyBeanConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class MyBeansConfig {
+public class MyBeanConfig {
 
   @Bean
   @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
