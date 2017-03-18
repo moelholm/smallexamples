5d8ae65dc8c8a9b04bf065e7a1666874d0c12bf1
Nicky Mølholm <nicky.moelholm@LEGO.com>
2017-03-18 21:57:13 +0100

    tidy up

--- springboot-kotlin-playground/src/test/kotlin/com/moelholm/MessageClientIntegrationTests.kt
+++ springboot-kotlin-playground/src/test/kotlin/com/moelholm/MessageClientIntegrationTests.kt
@@ -5,13 +5,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT
 import org.springframework.test.context.junit4.SpringRunner
 import kotlin.system.measureTimeMillis
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-        value = "server.port=8090")
+@SpringBootTest(webEnvironment = DEFINED_PORT, value = "server.port=8090")
 class MessageClientIntegrationTests {
 
     @Autowired
--- springboot-kotlin-playground/src/test/kotlin/com/moelholm/MessageControllerIntegrationTests.kt
+++ springboot-kotlin-playground/src/test/kotlin/com/moelholm/MessageControllerIntegrationTests.kt
@@ -5,11 +5,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class MessageControllerIntegrationTests {
 
     @Autowired lateinit
