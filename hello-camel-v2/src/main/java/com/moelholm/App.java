65e7b4c983409fd37c952c955da9f82f4113a8f5
Nicky Mølholm <nicky.moelholm@LEGO.com>
2017-08-19 15:57:51 +0200

    added example with JSON marshalling/unmarshalling

---
+++ hello-camel-v2/src/main/java/com/moelholm/App.java
@@ -0,0 +1,31 @@
+package com.moelholm;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    CamelContext context = new DefaultCamelContext();
+    context.addRoutes(new RouteBuilder() {
+      public void configure() {
+        from("file:data/inbox?noop=true")
+            .unmarshal()
+            .json(JsonLibrary.Gson)
+            .process(exchange -> {
+              System.out.format("  %s%n", exchange.getIn().getBody());
+            })
+            .filter(body().convertTo(String.class).contains("uke"))
+            .marshal()
+            .json(JsonLibrary.Gson)
+            .to("file:data/outbox");
+      }
+    });
+    context.start();
+    Thread.sleep(5000);
+    context.stop();
+  }
+
+}
