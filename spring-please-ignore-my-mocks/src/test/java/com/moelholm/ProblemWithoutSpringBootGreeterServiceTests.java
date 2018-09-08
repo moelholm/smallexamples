9232192292639c92bf94a89ef546fc6be85c92e1
moelholm <moelholm@gmail.com>
2018-09-08 14:11:45 +0200

    fixed bug in tests

--- spring-please-ignore-my-mocks/src/test/java/com/moelholm/ProblemWithoutSpringBootGreeterServiceTests.java
+++ spring-please-ignore-my-mocks/src/test/java/com/moelholm/ProblemWithoutSpringBootGreeterServiceTests.java
@@ -8,13 +8,13 @@ import com.moelholm.ProblemWithoutSpringBootGreeterServiceTests.BadTestConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = BadTestConfig.class)
+@ContextConfiguration(classes = BadTestConfig.class)
 public class ProblemWithoutSpringBootGreeterServiceTests {
 
   @Autowired
--- spring-please-ignore-my-mocks/src/test/java/com/moelholm/SolutionWithoutSpringBootGreeterServiceTests.java
+++ spring-please-ignore-my-mocks/src/test/java/com/moelholm/SolutionWithoutSpringBootGreeterServiceTests.java
@@ -10,13 +10,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = AwesomeTestConfig.class)
+@ContextConfiguration(classes = AwesomeTestConfig.class)
 public class SolutionWithoutSpringBootGreeterServiceTests {
 
   @Autowired
